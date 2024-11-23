641c8ec2c4672c89fd0b7031700b42a33236b14f
Wangqingran <wroolfd@gmail.com>
2024-11-23 16:04:19 +0800

    change backend code

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,1 +1,10 @@
- 
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(80))
+    phone = db.Column(db.String(11), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.nickname}>' 
--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql://mikechen_user:your_password_here@localhost/mikechen_db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///users.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
